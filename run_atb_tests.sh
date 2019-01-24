30ca976c8929a7da81f14df7d98936919b421e06
Sumit Anantwar <sumit.anantwar@gmail.com>
2019-01-24 23:17:01 +0200

    add Bash Script header to test scripts

--- run_atb_tests.sh
+++ run_atb_tests.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 carthage bootstrap --platform iOS --cache-builds;
 xcrun simctl uninstall booted com.duckduckgo.mobile.ios;
 xcodebuild test -quiet -project DuckDuckGo.xcodeproj -scheme AtbIntegrationTests CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone 6s';
--- run_site_report.sh
+++ run_site_report.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 carthage bootstrap --platform iOS --cache-builds;
 xcrun simctl uninstall booted com.duckduckgo.mobile.ios;
 xcodebuild test -project DuckDuckGo.xcodeproj -scheme TopSitesReport CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone 6s';
